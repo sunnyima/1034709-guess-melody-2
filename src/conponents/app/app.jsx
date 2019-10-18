daa14bf0575c10dad78c6123ab80c9a774f08eec
Marina Khokkonen <mariskasl@mail.ru>
2019-10-18 17:21:56 +0300

    first components

---
+++ src/conponents/app/app.jsx
@@ -0,0 +1,7 @@
+import WelcomeScreen from '../welcome-screen/welcome-screen';
+const App = () => {
+  // eslint-disable-next-line react/react-in-jsx-scope
+  return <WelcomeScreen />;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './conponents/app/app';
 
+const init = () => {
+  ReactDOM.render(
+      <App/>,
+      document.querySelector(`#root`)
+  );
+};
+
+init();
