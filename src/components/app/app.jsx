af0426fe92b95fe18f109a3d046ca623b8ada671
Marina Khokkonen <mariskasl@mail.ru>
2019-10-21 16:39:48 +0300

    6. Учебный проект: валидируй, как никогда не валидировал

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-const App = () =>{
+import PropTypes from 'prop-types';
+
+const App = (props) =>{
   return (
     <div>
-        Hello!
+      <p>Время на игру : {props.time}</p>
+      <p>Количество возможных ошибок : {props.mistakes}</p>
     </div>
   );
+};
+
+App.propTypes = {
+  mistakes: PropTypes.number.isRequired,
+  time: PropTypes.number.isRequired,
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import App from './components/app/app';
 
 const init = () => {
-  ReactDOM.render(<App />, document.querySelector(`#root`));
+  const settings = {
+    gameTime: 7,
+    errorCount: 4,
+  };
+  ReactDOM.render(<App
+    mistakes = {settings.errorCount}
+    time = {settings.gameTime}
+  />, document.querySelector(`#root`));
+};
+
+init.propTypes = {
+  settings: PropTypes.shape(
+      {
+        gameTime: PropTypes.number.isRequired,
+        errorCount: PropTypes.number.isRequired,
+      }),
 };
 
 init();
